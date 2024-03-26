0ac929de852d76160a82ec9718ffb0a6cabb2281
diogo-has <dioglois.games@gamil.com>
2024-03-26 12:23:44 -0300

    tilte

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,37 @@ import img1 from "./assets/img/img1.jpg"
 import img2 from "./assets/img/img2.jpg"
 import img3 from "./assets/img/img3.webp"
 
+import { Tilt } from 'react-tilt'
+
 function App() {
   return (
     <div className={style.section1}>
-      <Card
-        title='Paisage'
-        image={img1}
-        text1='birolaro'
-        text2='8329472943'
-      />
-      <Card
-        title='Paisage2'
-        image={img2}
-        text1='birolaro'
-        text2='8329472943'
-      />
-      <Card
-        title='Paisage2'
-        image={img3}
-        text1='birolaro'
-        text2='8329472943'
-      />
+      <Tilt>
+        <Card
+          title='Paisage'
+          image={img1}
+          text1='birolaro'
+          text2='8329472943'
+        />
+      </Tilt>
+      <Tilt>
+        <Card
+          title='Paisage2'
+          image={img2}
+          text1='birolaro'
+          text2='8329472943'
+        />
+
+      </Tilt>
+      <Tilt>
+        <Card
+          title='Paisage2'
+          image={img3}
+          text1='birolaro'
+          text2='8329472943'
+        />
+
+      </Tilt>
     </div>
   )
 }
